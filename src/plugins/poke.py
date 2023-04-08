54d2193e2c6529cb4839087fa63c782d948650b0
BioHazard <3482991796@qq.com>
2023-04-08 18:45:57 +0800

    modified:   src/plugins/poke.py

--- src/plugins/poke.py
+++ src/plugins/poke.py
@@ -1,19 +1,19 @@
 from nonebot import on_notice
-from nonebot.adapters.onebot.v11 import Message, Bot, MessageSegment, PokeNotifyEvent
+from nonebot.adapters.onebot.v11 import Bot, MessageSegment, PokeNotifyEvent
 from typing import NoReturn
-# from nonebot.rule import Rule
+from nonebot.rule import Rule
 
 
-# @Rule
-# async def poke_rule(bot: Bot, event: PokeNotifyEvent) -> bool:
-#     return event.target_id == int(bot.self_id)
+@Rule
+async def not_poked_by_self(event: PokeNotifyEvent) -> bool:
+    return event.user_id != event.self_id
 
-poke = on_notice(priority=10, block=False)
+poke = on_notice(rule=not_poked_by_self, priority=10, block=False)
 
 
 @poke.handle()
-async def poke_func(bot: Bot, event: PokeNotifyEvent) -> NoReturn:
-    if event.target_id == int(bot.self_id):
+async def poke_func(event: PokeNotifyEvent) -> NoReturn:
+    if event.target_id == event.self_id:  # 别人戳自己
         await poke.finish(MessageSegment('poke', {'qq': event.user_id}))
-    else:
+    else:  # 别人戳别人
         await poke.finish(MessageSegment('poke', {'qq': event.target_id}))
