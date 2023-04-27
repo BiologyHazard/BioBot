cad6e0a5c819420193304dce40a6e224c995fd0a
BioHazard <3482991796@qq.com>
2023-04-27 13:10:13 +0800

    modified:   src/plugins/answersbook/__init__.py

--- src/plugins/answersbook/__init__.py
+++ src/plugins/answersbook/__init__.py
@@ -2,9 +2,9 @@ import random
 from pathlib import Path
 
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Message, MessageEvent
+from nonebot.adapters.onebot.v11 import Message, MessageEvent, MessageSegment
 from nonebot.matcher import Matcher
-from nonebot.params import CommandArg
+from nonebot.params import CommandArg, EventMessage
 from nonebot.typing import T_State
 
 answers_path: Path = Path(__file__).parent / "answersbook.txt"
@@ -19,13 +19,23 @@ look_answer: type[Matcher] = on_command("翻看答案")
 
 
 @look_answer.handle()
-async def answersbook(state: T_State, event: MessageEvent, message: Message = CommandArg()) -> None:
+async def answersbook(matcher: Matcher,
+                      state: T_State,
+                      event: MessageEvent,
+                      command_arg: Message = CommandArg(),
+                      event_message: Message = EventMessage()) -> None:
     state['user_id'] = event.user_id
-    if message.extract_plain_text():
-        state['has_arg'] = True
+    if event_message[0].type == 'reply':
+        state['reply'] = event_message[0].data['id']
+    if command_arg.extract_plain_text():
+        matcher.set_arg('question', command_arg)
 
 
-@look_answer.got('has_arg', Message.template('{user_id:at}你想问什么问题呢？'))
-async def anwsersbook() -> None:
+@look_answer.got('question', Message.template('{user_id:at}你想问什么问题呢？'))
+async def anwsersbook(state: T_State, event: MessageEvent) -> None:
     answer: str = get_answers()
-    await look_answer.finish(answer, reply_message=True)
+    if 'reply' in state:
+        reply: int = state['reply']
+    else:
+        reply = event.message_id
+    await look_answer.finish(Message([MessageSegment.reply(reply), MessageSegment.text(answer)]))
