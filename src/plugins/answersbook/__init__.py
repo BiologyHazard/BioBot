c7aacf62de7247b3ee8c3c732fe2687c739bc6dc
BioHazard <3482991796@qq.com>
2023-04-27 12:54:15 +0800

    modified answersbook

--- src/plugins/answersbook/__init__.py
+++ src/plugins/answersbook/__init__.py
@@ -2,7 +2,7 @@ import random
 from pathlib import Path
 
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Message
+from nonebot.adapters.onebot.v11 import Message, MessageEvent
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
 from nonebot.typing import T_State
@@ -19,12 +19,13 @@ look_answer: type[Matcher] = on_command("翻看答案")
 
 
 @look_answer.handle()
-async def answersbook(state: T_State, message: Message = CommandArg()) -> None:
+async def answersbook(state: T_State, event: MessageEvent, message: Message = CommandArg()) -> None:
+    state['user_id'] = event.user_id
     if message.extract_plain_text():
         state['has_arg'] = True
 
 
-@look_answer.got('has_arg', '你想问什么问题呢？')
+@look_answer.got('has_arg', Message.template('{user_id:at}你想问什么问题呢？'))
 async def anwsersbook() -> None:
     answer: str = get_answers()
     await look_answer.finish(answer, reply_message=True)
