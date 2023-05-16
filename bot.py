a32140db4367b3065448df71e81847816efa6cc3
BioHazard <3482991796@qq.com>
2023-05-16 21:38:43 +0800

    add plugin ncm

--- bot.py
+++ bot.py
@@ -35,6 +35,7 @@ def main() -> None:
     nonebot.load_plugin('src.plugins.answersbook')
     nonebot.load_plugin('src.plugins.tygj')
     nonebot.load_plugin('src.plugins.revoke')
+    nonebot.load_plugin('src.plugins.ncm')
     # nonebot.load_plugin('src.plugins.wordcloud')
 
     nonebot.load_plugin('nonebot_plugin_emojimix')
