36f9cb5f986f665985b17f3ecb7b01a24d221cb2
BioHazard <3482991796@qq.com>
2025-03-09 20:33:11 +0800

    bot basic

--- bot.py
+++ bot.py
@@ -1,53 +1,72 @@
+import sys
+
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
 from nonebot.drivers import Driver
-from nonebot.log import default_filter, default_format, logger
+from nonebot.log import default_filter, logger
+
+logger_format: str = (
+    '<dim>File <cyan>"{file.path}"</>, line <cyan>{line}</>, in <cyan>{function}</></>'
+    "\n"
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</> "
+    "[<level>{level}</>] "
+    # "<cyan><underline>{name}</></>:"
+    # "<cyan>{function}</>:<cyan>{line}</> | "
+    "<level><normal>{message}</></>"
+)
 
 
 def main() -> None:
+    logger.remove()
+    logger.add(sys.stderr,
+               level=0,
+               format=logger_format,
+               filter=default_filter)
     logger.add('logs/bot_{time:YYYY-MM-DD}.log',
                level=0,
                rotation='00:00',
-               format=default_format,
+               format=logger_format,
                filter=default_filter)
+
     nonebot.init()
 
     driver: Driver = nonebot.get_driver()
     driver.register_adapter(OneBotV11Adapter)
+    nonebot.load_from_toml('pyproject.toml')
 
     nonebot.load_builtin_plugins('echo')
     nonebot.load_plugin('src.plugins.help')
-    nonebot.load_plugin('src.plugins.blacklist')
-    nonebot.load_plugin('src.plugins.fwdrequests')
-    nonebot.load_plugin('src.plugins.runcode')
+    # nonebot.load_plugin('src.plugins.blacklist')
+    # nonebot.load_plugin('src.plugins.fwdrequests')
+    # nonebot.load_plugin('src.plugins.runcode')
     nonebot.load_plugin('src.plugins.sklassistant')
-    nonebot.load_plugin('src.plugins.chat')
-    nonebot.load_plugin('src.plugins.maimai')
+    # nonebot.load_plugin('src.plugins.chat')
+    # nonebot.load_plugin('src.plugins.maimai')
     nonebot.load_plugin('src.plugins.autoreply')
-    nonebot.load_plugin('src.plugins.slscq')
-    nonebot.load_plugin('src.plugins.biliinfo')
-    nonebot.load_plugin('src.plugins.roll')
-    nonebot.load_plugin('src.plugins.poke')
-    nonebot.load_plugin('src.plugins.simplemusic')
+    # nonebot.load_plugin('src.plugins.slscq')
+    # nonebot.load_plugin('src.plugins.biliinfo')
+    # nonebot.load_plugin('src.plugins.roll')
+    # nonebot.load_plugin('src.plugins.poke')
+    # nonebot.load_plugin('src.plugins.simplemusic')
     nonebot.load_plugin('src.plugins.repeater')
-    nonebot.load_plugin('src.plugins.boardgame')
-    nonebot.load_plugin('src.plugins.text2sound')
+    # nonebot.load_plugin('src.plugins.boardgame')
+    # nonebot.load_plugin('src.plugins.text2sound')
     nonebot.load_plugin('src.plugins.xxivgame')
     nonebot.load_plugin('src.plugins.homo')
     nonebot.load_plugin('src.plugins.mahjong')
     nonebot.load_plugin('src.plugins.answersbook')
-    nonebot.load_plugin('src.plugins.tygj')
+    # nonebot.load_plugin('src.plugins.tygj')
     nonebot.load_plugin('src.plugins.revoke')
-    nonebot.load_plugin('src.plugins.ncm')
+    # nonebot.load_plugin('src.plugins.ncm')
     nonebot.load_plugin('src.plugins.abbreply')
     nonebot.load_plugin('src.plugins.kfccrazythu')
     nonebot.load_plugin('src.plugins.choose')
-    nonebot.load_plugin('src.plugins.wordle')
-    nonebot.load_plugin('src.plugins.handle')
+    # nonebot.load_plugin('src.plugins.wordle')
+    # nonebot.load_plugin('src.plugins.handle')
 
-    nonebot.load_plugin('nonebot_plugin_emojimix')
-    nonebot.load_plugin('nonebot_plugin_abstract')
-    nonebot.load_plugin('nonebot_plugin_makemidi')
+    # nonebot.load_plugin('nonebot_plugin_emojimix')
+    # nonebot.load_plugin('nonebot_plugin_abstract')
+    # nonebot.load_plugin('nonebot_plugin_makemidi')
     # nonebot.load_plugin('nonebot_plugin_wordcloud')
     # nonebot.load_plugin('nonebot_plugin_memes')
 
