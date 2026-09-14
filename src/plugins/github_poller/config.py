7a6a26aed7ea65e03ee704be8895b3716199147f
BioHazard <3482991796@qq.com>
2026-09-14 22:48:46 +0800

    refactor: 移除写得不好的 github_poller

--- src/plugins/github_poller/config.py
+++ src/plugins/github_poller/config.py
@@ -1,14 +1,11 @@
-from pathlib import Path
-
 from nonebot import get_plugin_config
 from pydantic import BaseModel, Field
 
 
 class Config(BaseModel):
-    github_token: str | None = None
-    github_poll_interval: int = Field(default=300, ge=10)
-    github_poller_data_path: Path = Path("data/github_poller/data.json")
-    github_poller_per_page: int = Field(default=30, ge=1, le=100)
+    github_poller_github_token: str | None = None
+    github_poller_poll_interval: float = Field(default=300, ge=10)
+    github_poller_user_agent: str = "BioBot"
 
 
 plugin_config = get_plugin_config(Config)
